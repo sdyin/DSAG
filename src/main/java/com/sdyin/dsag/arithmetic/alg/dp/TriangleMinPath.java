0de0d88c65fa312046b665417759f80b04e0185b
liuye <liuye@xforceplus.com>
2020-07-31 18:09:26 +0800

    dp 三角形最小路径之和 demo(待完成)

---
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/dp/TriangleMinPath.java
@@ -0,0 +1,33 @@
+package com.sdyin.dsag.arithmetic.alg.dp;
+
+import java.util.List;
+
+/**
+ * @Description 三角形最小路径和
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ *
+ * 例如：
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ *
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）
+ *
+ * @Author liuye
+ * @Date 2020/7/31 16:22
+ **/
+public class TriangleMinPath {
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
