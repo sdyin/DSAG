350e5ca4bcd1eff0f1316295e5d17cd7e9104a6b
liuye <liuye@xforceplus.com>
2020-08-27 17:40:38 +0800

    guavaBloomFilter 添加

---
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/bloomfilter/guavaBloomFilter.java
@@ -0,0 +1,28 @@
+package com.sdyin.dsag.arithmetic.alg.bloomfilter;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import com.sdyin.dsag.arithmetic.alg.Test;
+
+/**
+ * @Description guava提供的布隆过滤器
+ * 缺点是只能单机使用,数据放在本地内存
+ * @Author liuye
+ * @Date 2020/8/27 11:03
+ **/
+public class guavaBloomFilter {
+
+    public static void main(String[] args) {
+        //Funnels.stringFunnel(Charsets.UTF_8);
+        BloomFilter<Integer> filter = BloomFilter.create(Funnels.integerFunnel(), 5000, 0.01);
+        // 判断指定元素是否存在
+        System.out.println(filter.mightContain(1));
+        System.out.println(filter.mightContain(2));
+        // 将元素添加进布隆过滤器
+        filter.put(1);
+        filter.put(2);
+        System.out.println(filter.mightContain(1));
+        System.out.println(filter.mightContain(2));
+    }
+}
