a19cd18894c2a986487bf3aa3705f43fc8c5dd7b
liuye <liuye@xforceplus.com>
2020-09-26 19:21:29 +0800

    路径总和 补充bfs解法

--- src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/PathSum2.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/PathSum2.java
@@ -4,6 +4,7 @@ import lombok.val;
 import org.checkerframework.checker.units.qual.K;
 import sun.security.krb5.internal.crypto.Aes128;
 
+import javax.jws.WebParam;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -25,7 +26,11 @@ public class PathSum2 {
         return ret;
     }
 
-
+    /**
+     * dfs 深度优先搜索
+     * @param root
+     * @param sum
+     */
     private void dfs(TreeNode root, int sum) {
         if(root == null){
             return;
@@ -40,6 +45,77 @@ public class PathSum2 {
         //删除该节点，即删除非叶子节点
         path.pollLast();
     }
+
+    /**
+     * bfs 广度优先搜素
+     * 一行一行遍历
+     * @param treeNode
+     * @param sum
+     */
+    private List<List<Integer>> pathSumBfs(TreeNode root, int sum){
+
+        List<List<Integer>> res = new ArrayList<>();
+        //如果节点为空直接返回
+        if (root == null){
+            return res;
+        }
+
+        //使用两个队列，一个存储结点，一个存储从跟结点到当前节点的路径
+        Queue<TreeNode> queueNode = new LinkedList<>();
+        Queue<List<Integer>> queueList = new LinkedList<>();
+
+        //跟节点入队
+        queueNode.add(root);
+        //根节点的路径入队(每个元素为集合)
+        List<Integer> list = new ArrayList<>();
+        list.add(root.val);
+        queueList.add(list);
+
+        while (!queueNode.isEmpty()) {
+            //当前节点出队
+            TreeNode node  = queueNode.poll();
+            //当前节点的路径出队
+            List<Integer> tempList = queueList.poll();
+
+            //满足条件，添加路径到响应
+            if(node.left == null && node.right == null && node.val == sum){
+                res.add(tempList);
+            }
+
+            //左子节点不为空
+            if(node.left != null){
+                //路径入队
+                tempList.add(node.left.val);
+                queueList.add(new ArrayList<>(tempList));
+
+                //节点累计路径和
+                node.left.val += node.val;
+                //节点入队
+                queueNode.add(node.left);
+                //删除左节点路径，左节点路径不能和右节点累加
+                tempList.remove(tempList.size() - 1);
+
+            }
+
+            //右节点不为空
+            if(node.right != null){
+                //路径入队
+                tempList.add(node.right.val);
+                queueList.add(new ArrayList<>(tempList));
+
+                //节点累计路径和
+                node.right.val += node.val;
+                //节点入队
+                queueNode.add(node.right);
+            }
+
+        }
+
+        return res;
+
+    }
+
+
 
 }
 
