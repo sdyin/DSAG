9968aefebb6228fa49da780451bbc155783c1737
liuye <liuye@xforceplus.com>
2020-09-30 20:57:57 +0800

    插入排序 补充

--- src/main/java/com/sdyin/dsag/arithmetic/alg/sort/InsertSort.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/sort/InsertSort.java
@@ -19,11 +19,11 @@ public class InsertSort {
 
         for (int i = 1; i < sourceArr.length; i++) {
             int data = sourceArr[i];
-            for (int j = i-1; j >= 0; j--) {
-                if(j == 0 && data < sourceArr[j]){
+            for (int j = i - 1; j >= 0; j--) {
+                if (j == 0 && data < sourceArr[j]) {
                     sourceArr = insert(sourceArr, i, j);
                 }
-                if(j > 0 && data < sourceArr[j] && data >= sourceArr[j - 1]){
+                if (j > 0 && data < sourceArr[j] && data >= sourceArr[j - 1]) {
                     sourceArr = insert(sourceArr, i, j);
                 }
             }
@@ -33,6 +33,7 @@ public class InsertSort {
 
     /**
      * 插入指定位置
+     *
      * @param sourceArr
      * @param i
      * @param j
@@ -41,7 +42,7 @@ public class InsertSort {
     private static int[] insert(int[] sourceArr, int i, int j) {
         int data = sourceArr[i];
         for (int k = i; k > j; k--) {
-            sourceArr[k] = sourceArr[k-1];
+            sourceArr[k] = sourceArr[k - 1];
         }
         sourceArr[j] = data;
         return sourceArr;
@@ -49,9 +50,38 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int[] arrays = {1, 34, 56, 22, 8, 1, 46, 99};
-        int[] result = sort(arrays);
+        int[] result = insertSort(arrays);
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
         }
+    }
+
+
+    /**
+     * 插入排序方式二
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] insertSort(int[] arr) {
+        // 从下标为1的元素开始选择合适的位置插入，因为下标为0的只有一个元素，默认是有序的
+        for (int i = 1; i < arr.length; i++) {
+
+            // 记录要插入的数据
+            int tmp = arr[i];
+
+            // 从已经排序的序列最右边的开始比较，找到比其小的数
+            int j = i;
+            while (j > 0 && tmp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+
+            // 存在比其小的数，插入
+            if (j != i) {
+                arr[j] = tmp;
+            }
+        }
+        return arr;
     }
 }
