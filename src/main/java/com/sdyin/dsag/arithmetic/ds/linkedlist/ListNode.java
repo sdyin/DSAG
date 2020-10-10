5c64adfe805b89178da301461584f5418cf8e3c7
liuye <liuye@xforceplus.com>
2020-10-10 15:20:55 +0800

    环形链表查找环形起点

---
+++ src/main/java/com/sdyin/dsag/arithmetic/ds/linkedlist/ListNode.java
@@ -0,0 +1,16 @@
+package com.sdyin.dsag.arithmetic.ds.linkedlist;
+
+/**
+ * @Description 链表
+ * @Author liuye
+ * @Date 2020/10/10 15:11
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
