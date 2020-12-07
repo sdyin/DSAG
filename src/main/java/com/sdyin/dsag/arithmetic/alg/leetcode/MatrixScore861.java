e1b6c44e793b47906d124dd1230295bf147e00fb
liuye <sdyin@foxmail.com>
2020-12-07 19:02:18 +0800

    leetcode 861. 翻转矩阵后的得分

---
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/MatrixScore861.java
@@ -0,0 +1,58 @@
+package com.sdyin.dsag.arithmetic.alg.leetcode;
+
+/**
+ * @Description: leetcode 861. 翻转矩阵后的得分
+ * 有一个二维矩阵A 其中每个元素的值为0或1。
+ *
+ * 移动是指选择任一行或列，并转换该行或列中的每一个值：将所有 0 都更改为 1，将所有 1 都更改为 0。
+ *
+ * 在做出任意次数的移动后，将该矩阵的每一行都按照二进制数来解释，矩阵的得分就是这些数字的总和。
+ *
+ * 返回尽可能高的分数。
+ *
+ * 示例：
+ *
+ * 输入：[[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+ * 输出：39
+ * 解释：
+ * 转换为 [[1,1,1,1],[1,0,0,1],[1,1,1,1]]
+ * 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+ *
+ * 示例：
+ *
+ * 输入：[[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+ * 输出：39
+ * 解释：
+ * 转换为 [[1,1,1,1],[1,0,0,1],[1,1,1,1]]
+ * 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/score-after-flipping-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: liuye
+ * @time: 2020/12/7$ 下午6:13$
+ */
+public class MatrixScore861 {
+
+    public int matrixScore(int[][] A) {
+        int m = A.length, n = A[0].length;
+        //TODO 没理解
+        int ret = m * (1 << (n - 1));
+
+        for (int j = 1; j < n; j++) {
+            int nOnes = 0;
+            for (int i = 0; i < m; i++) {
+                if (A[i][0] == 1) {
+                    nOnes += A[i][j];
+                } else {
+                    // 如果这一行进行了行反转，则该元素的实际取值为 1 - A[i][j]
+                    nOnes += (1 - A[i][j]);
+                }
+            }
+            int k = Math.max(nOnes, m - nOnes);
+            ret += k * (1 << (n - j - 1));
+        }
+        return ret;
+    }
+}
