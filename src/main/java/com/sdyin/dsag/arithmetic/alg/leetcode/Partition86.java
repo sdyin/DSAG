e0ca74b817a97050f6135ded1c0da9ed75a9c96d
liuye <sdyin@foxmail.com>
2021-01-04 22:35:38 +0800

    86. 分隔链表

---
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/Partition86.java
@@ -0,0 +1,39 @@
+package com.sdyin.dsag.arithmetic.alg.leetcode;
+
+/**
+ * @Description: 86. 分隔链表
+ * 给你一个链表和一个特定值 x ，请你对链表进行分隔，使得所有小于 x 的节点都出现在大于或等于 x 的节点之前。
+ *
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/partition-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author: liuye
+ * @time: 2021/1/3$ 下午8:51$
+ */
+public class Partition86 {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+
+        while (head != null){
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        //大链表尾结点置空
+        large.next = null;
+        //连接大链表小链表
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+}
