d8ac5e48d0e43afd89928053349fc5cc6f45d130
liuye <liuye@xforceplus.com>
2021-01-20 22:11:01 +0800

    leetcode 628. 三个数的最大乘积

---
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/MaximumProduct628.java
@@ -0,0 +1,40 @@
+package com.sdyin.dsag.arithmetic.alg.leetcode;
+
+import sun.security.krb5.internal.crypto.Aes128CtsHmacSha1EType;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @Description
+ * 628. 三个数的最大乘积
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ * @Author liuye
+ * @Date 2021/1/20 21:37
+ **/
+public class MaximumProduct628 {
+
+    /**
+     * 如果数组中全是非负数，则排序后最大的三个数相乘即为最大乘积；如果全是非正数，则最大的三个数相乘同样也为最大乘积。
+     *
+     * 如果数组中有正数有负数，则最大乘积既可能是三个最大正数的乘积，也可能是两个最小负数（即绝对值最大）与最大正数的乘积。
+     *
+     * 综上，我们在给数组排序后，分别求出三个最大正数的乘积，以及两个最小负数与最大正数的乘积，二者之间的最大值即为所求答案。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/maximum-product-of-three-numbers/solution/san-ge-shu-de-zui-da-cheng-ji-by-leetcod-t9sb/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @return
+     */
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+    }
+}
