7591bb5e3d3d2d6b7c34c1cc19284daad664df0f
刘烨 <liuye@xforceplus.com>
2022-03-19 16:04:50 +0800

    二叉树最大深度dfs，bfs解法提交

--- src/main/java/com/sdyin/dsag/arithmetic/alg/dfs/TreeMaxDeep.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/dfs/TreeMaxDeep.java
@@ -15,7 +15,6 @@ import java.util.ArrayDeque;
 public class TreeMaxDeep {
 
     public int maxDepth(TreeNode root) {
-        //int deep = dfs(root, 0);
         int deep = bfs(root);
         return deep;
     }
