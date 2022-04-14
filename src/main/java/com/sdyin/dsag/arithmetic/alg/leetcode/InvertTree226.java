fe095aea0a1d3d97fb88e80d35ea7026a26a60ce
刘烨 <liuye@xforceplus.com>
2022-04-14 23:38:30 +0800

    leetcode 226. 翻转二叉树,备注调整

--- src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/InvertTree226.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/InvertTree226.java
@@ -15,19 +15,18 @@ public class InvertTree226 {
      * @param root
      * @return
      */
-    public TreeNode invertTree(TreeNode root) {
+    public   TreeNode invertTree(TreeNode root) {
         if ( root == null) {
             return root;
         }
+        //前序遍历，翻转左右子节点
+        //不可以中序遍历，中序会导致，先处理左子节点，再处理当前节点，再处理右节点(此时右节点就是原始节点的左节点,被翻转了)
         TreeNode node = root.left;
         root.left = root.right;
         root.right = node;
-        if (root.left != null) {
-            invertTree(root.left);
-        }
-        if (root.right != null) {
-            invertTree(root.right);
-        }
+
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
 }
