1e8542700171e2baf64955d19e91c1cf9d634a87
刘烨 <liuye@xforceplus.com>
2022-05-26 22:38:28 +0800

    leetcode 226. 翻转二叉树调整

--- src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/InvertTree226.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/InvertTree226.java
@@ -3,20 +3,19 @@ package com.sdyin.dsag.arithmetic.alg.leetcode;
 /**
  * @Description: 226. 翻转二叉树
  * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
- *
  * @Author: liuye
  * @time: 2022/4/14$ 11:13 上午$
  */
 public class InvertTree226 {
 
     /**
-     * 调换左右节点即可
+     * 遍历递归的思路： 调换左右节点即可
      *
      * @param root
      * @return
      */
-    public   TreeNode invertTree(TreeNode root) {
-        if ( root == null) {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
             return root;
         }
         //前序遍历，翻转左右子节点
@@ -27,6 +26,28 @@ public class InvertTree226 {
 
         invertTree(root.left);
         invertTree(root.right);
+        return root;
+    }
+
+    /**
+     * 分解问题的思路
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        // 利用函数定义，先翻转左右子树
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        // 然后交换左右子节点
+        root.left = right;
+        root.right = left;
+
+        // 和定义逻辑自恰：以 root 为根的这棵二叉树已经被翻转，返回 root
         return root;
     }
 }
