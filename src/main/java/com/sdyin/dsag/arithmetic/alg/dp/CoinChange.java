8b83d89f948d52ac7bdaf3b3d17293bfa9665b47
刘烨 <liuye@xforceplus.com>
2022-10-28 15:43:25 +0800

    leetcode dp 零钱调整

--- src/main/java/com/sdyin/dsag/arithmetic/alg/dp/CoinChange.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/dp/CoinChange.java
@@ -18,20 +18,30 @@ public class CoinChange {
         System.out.println("结果："+ result);
     }
 
+    /**
+     * 迭代解法 -》 自底向上
+     * @param coins
+     * @param amount
+     * @return
+     */
     public static int coinChange(int[] coins, int amount){
-        int[] dp = new int[amount+1];
-        //初始化值,初始化一个最大可能值+1，最终不存在则为此值 不会更新
-        Arrays.fill(dp, amount+1);
+        int[] dp = new int[amount + 1];
+        // 数组大小为 amount + 1，初始值也为 amount + 1 -> 方便后续Math.min
+        Arrays.fill(dp, amount + 1);
+        //dp[i] 的值表示数额为i时，最小的硬币数量
+        // base case
         dp[0] = 0;
-        for (int i = 0; i < dp.length; i++){
-            for (int coin: coins) {
-                //无解
-                if(i - coin < 0){
+        // 遍历所有场景
+        for (int i = 0; i < dp.length; i++) {
+            //所有场景的取值
+            for (int coin: coins){
+                // 子问题无解，跳过 -> 最小coins都大于目标amount场景
+                if (i - coin < 0) {
                     continue;
                 }
                 dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                System.out.println("dp[i]:" + i + " result:" + dp[i]);
             }
-            System.out.println(i + ":" + dp[i]);
         }
         return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
