80129bdf0f8925c0fa60f80a7a8009904e5e3289
刘烨 <liuye@xforceplus.com>
2023-02-20 14:49:56 +0800

    冒泡排序调整

--- src/main/java/com/sdyin/dsag/arithmetic/alg/sort/BubbleSort.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/sort/BubbleSort.java
@@ -11,11 +11,11 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {12,66,33,23,78,33,68};
+        int[] arr = {12, 66, 33, 23, 78, 33, 68};
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
