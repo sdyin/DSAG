194fa0e3b5e0b5eb16b24c7508c22570ff7c4e16
刘烨 <liuye@xforceplus.com>
2023-07-10 09:34:42 +0800

    leetcode 92. 反转链表 II 注释调整

--- src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/ReverseBetween92.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/ReverseBetween92.java
@@ -10,6 +10,14 @@ package com.sdyin.dsag.arithmetic.alg.leetcode;
  */
 public class ReverseBetween92 {
 
+    /**
+     * 迭代解法
+     *
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
@@ -17,28 +25,29 @@ public class ReverseBetween92 {
         ListNode g = dummyHead;
         ListNode q = dummyHead.next;
 
+        // 为什么这里是 < 不是 <=: 因为left 是从1开始的
+        // 为什么这里是left - 1: 因为循环是从(q节点)首节点开始，不是从首节点的前驱节点。
+        // (我们要反转区间链表，就要找到目标链表的第一个元素以及前驱元素)
         for (int i = 0; i < left - 1; i++) {
             g = g.next;
             q = q.next;
         }
 
         for (int i = 0; i < right - left; i++) {
+            // 暂存后一节点
             ListNode node = q.next;
 
-            //去除q的后一节点,实际上就是去除node,继续下一节点的插入
+            //去除q的后驱节点,实际上就是链表去除node节点
             //这一步放在循环最后一行会影响g节点
             q.next = q.next.next;
 
-            //g的后继节点指向node,更改node的后继节点
+            //就是把node节点(也就是原q的后驱节点)，插入到q链表的队头
             node.next =  g.next;
-            //node指向g, 配合上一步就是将node 插入g节点后
+            //g节点变更为调整后的链表队头
             g.next = node;
-
 
         }
 
-
         return dummyHead.next;
-
     }
 }
