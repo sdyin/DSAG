3d9ddb283d6a6bd5c0f2dd03b53ddab1e9f85f02
刘烨 <liuye@xforceplus.com>
2025-05-10 22:52:37 +0800

    15. 三数之和

--- src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/ThreeSum15.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/ThreeSum15.java
@@ -5,61 +5,81 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * @Description: leetcode 15.三数之和
- * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，
- * 使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * @Description:
+ * 15. 三数之和
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+ * 同时还满足 nums[i] + nums[j] + nums[k] == 0 。
+ * 请你返回所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 解释：
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ * 注意，输出的顺序和三元组的顺序并不重要。
+ * 输入：nums = [0,1,1]
+ * 输出：[]
+ * 解释：唯一可能的三元组和不为 0 。
+ * 输入：nums = [0,0,0]
+ * 输出：[[0,0,0]]
+ * 解释：唯一可能的三元组和为 0 。
+ * 提示：
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
  *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/3sum
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @Author: liuye
- * @time: 2022/2/11$ 3:49 下午$
+ * @time: 2025/5/9$ 18:00$
  */
 public class ThreeSum15 {
 
-    public static void main(String[] args) {
-        int[] nums = {-8, -4, -4, -2, 0, 2, 4, 8};
-        List<List<Integer>> lists = threeSum(nums);
-        System.out.println(lists);
-    }
-
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         if (nums == null || nums.length < 3) {
-            return list;
+            return result;
         }
-        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0){
-                return list;
+        // 排序
+        Arrays.sort(nums);
+        // 遍历数组
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 提前终止条件
+            if (nums[i] > 0) {
+                break;
             }
-            // 去重
-            if (i > 0 && nums[i] == nums[i - 1]){
+
+            // 跳过重复元素
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+            // 左右指针
             int left = i + 1;
             int right = nums.length - 1;
-            while(left < right) {
-                if (nums[i] + nums[left] + nums[right] < 0) {
-                    left++;
-                } else if (nums[i] + nums[left] + nums[right] > 0) {
-                    right--;
-                } else {
-                    List<Integer> sublist = Arrays.asList(nums[i], nums[left], nums[right]);
-                    list.add(sublist);
-                    // 去重
-                    while (left < right && nums[left+1] == nums[left]){
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 跳过重复元素
+                    while (left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
-                    while (left < right && nums[right-1] == nums[right]){
+                    while (left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
                     left++;
                     right--;
                 }
+                // 小于0，左指针右移；
+                if (sum < 0) {
+                    left++;
+                }
+                // 大于0，右指针左移
+                if (sum > 0) {
+                    right--;
+                }
             }
         }
-        return list;
+        return result;
     }
 }
