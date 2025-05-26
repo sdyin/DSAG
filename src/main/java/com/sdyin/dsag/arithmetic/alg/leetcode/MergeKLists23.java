8c21629f6d89d1398ca7868d0588c7621dd232e3
刘烨 <liuye@xforceplus.com>
2025-05-26 17:11:48 +0800

    leetcode 23. 合并K个升序链表。增加分治解法

--- src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/MergeKLists23.java
+++ src/main/java/com/sdyin/dsag/arithmetic/alg/leetcode/MergeKLists23.java
@@ -15,6 +15,13 @@ import java.util.PriorityQueue;
  */
 public class MergeKLists23 {
 
+    /**
+     * 优先队列解法
+     * 时间复杂度：O(kn*logk)
+     * 空间复杂度：O(k)
+     * @param lists
+     * @return
+     */
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0) {
             return null;
@@ -41,6 +48,52 @@ public class MergeKLists23 {
                 pq.add(node.next);
             }
             p = p.next;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 分治解法
+     * 时间复杂度：O(n*logk)
+     * 空间复杂度：O(logk), 递归调用栈
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null && lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while (l1!= null && l2!= null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1!= null) {
+            p.next = l1;
+        }
+        if (l2!= null) {
+            p.next = l2;
         }
         return dummy.next;
     }
